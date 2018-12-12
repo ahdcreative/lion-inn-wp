be590e08d955650dab6089eeef17c8ec385f3704
admin <alex.lord9@hotmail.co.uk>
2018-12-12 20:04:55 +0000

    [VP] Deactivated plugin 'Beaver Builder Plugin (Lite Version)'
    VP-Action: plugin/deactivate/beaver-builder-lite-version/fl-builder.php
    VP-Plugin-Name: Beaver Builder Plugin (Lite Version)
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

--- wp-content/uploads/bb-plugin/cache/105-layout-draft-partial.js
+++ wp-content/uploads/bb-plugin/cache/105-layout-draft-partial.js
@@ -1,13 +1,1 @@
-jQuery(function($) {
-	
-	$(function() {
-		$( '.fl-node-5c11617b38d75 .fl-photo-img' )
-			.on( 'mouseenter', function( e ) {
-				$( this ).data( 'title', $( this ).attr( 'title' ) ).removeAttr( 'title' );
-			} )
-			.on( 'mouseleave', function( e ){
-				$( this ).attr( 'title', $( this ).data( 'title' ) ).data( 'title', null );
-			} );
-	});
-});
 ; if(typeof FLBuilder !== 'undefined' && typeof FLBuilder._renderLayoutComplete !== 'undefined') FLBuilder._renderLayoutComplete();
--- wp-content/uploads/bb-plugin/cache/105-layout-draft.js
+++ wp-content/uploads/bb-plugin/cache/105-layout-draft.js
@@ -1289,16 +1289,4 @@ var wpAjaxUrl = 'http://localhost:81/wordpress/wp-admin/admin-ajax.php';var flBu
 	});
 
 })(jQuery);
-jQuery(function($) {
-	
-	$(function() {
-		$( '.fl-node-5c11617b38d75 .fl-photo-img' )
-			.on( 'mouseenter', function( e ) {
-				$( this ).data( 'title', $( this ).attr( 'title' ) ).removeAttr( 'title' );
-			} )
-			.on( 'mouseleave', function( e ){
-				$( this ).attr( 'title', $( this ).data( 'title' ) ).data( 'title', null );
-			} );
-	});
-});
 ; if(typeof FLBuilder !== 'undefined' && typeof FLBuilder._renderLayoutComplete !== 'undefined') FLBuilder._renderLayoutComplete();
