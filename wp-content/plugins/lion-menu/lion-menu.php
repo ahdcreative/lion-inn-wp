c8edda70dc59476774cba0fefca3de9987f493bc
Alex Lord <alex.lord9@hotmail.co.uk>
2019-01-06 16:48:32 +0000

    Created custom menu plugin. Installed another menu plugin to use as an example.

---
+++ wp-content/plugins/lion-menu/lion-menu.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Lion Menu
+Plugin URI:
+Description: Menu Plugin for Restuarants & Pubs
+Version: 1.0.0
+Author: Alexander Lord
+Author URI:
+*/
+
+// Exit if accessed directly
+if(!defined('ABSPATH')) {
+    exit;
+}
+
+// Load scripts
+require_once(plugin_dir_path(__FILE__).'/includes/lion-menu-scripts.php');
+
+?>
