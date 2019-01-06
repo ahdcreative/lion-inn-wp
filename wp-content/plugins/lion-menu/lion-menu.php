0a848dc7b24aced8503231899fe9077ffe7c9c33
admin <alex.lord9@hotmail.co.uk>
2019-01-06 17:10:04 +0000

    [VP] Activated plugin 'Lion Menu'
    VP-Action: plugin/activate/lion-menu/lion-menu.php
    VP-Plugin-Name: Lion Menu
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

--- wp-content/plugins/lion-menu/lion-menu.php
+++ wp-content/plugins/lion-menu/lion-menu.php
@@ -16,4 +16,15 @@ if(!defined('ABSPATH')) {
 // Load scripts
 require_once(plugin_dir_path(__FILE__).'/includes/lion-menu-scripts.php');
 
+// Add 'Menu' Option to Admin Menu
+add_action('admin_menu', 'admin_menu_option');
+
+function admin_menu_option(){
+    add_menu_page( 'Menu Page', 'Menu', 'manage_options', 'lion-menu-plugin', 'menu_init' );
+}
+
+function menu_init(){
+    echo "<h1>Menu Page!</h1>";
+}
+
 ?>
