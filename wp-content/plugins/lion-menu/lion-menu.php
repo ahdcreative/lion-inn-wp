74e6b4ae7bd74fb864a7e95d3457f61ca2270b0c
admin <alex.lord9@hotmail.co.uk>
2019-01-26 14:16:02 +0000

    [VP] Deactivated plugin 'Lion Menu'
    VP-Action: plugin/deactivate/lion-menu/lion-menu.php
    VP-Plugin-Name: Lion Menu
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

--- wp-content/plugins/lion-menu/lion-menu.php
+++ wp-content/plugins/lion-menu/lion-menu.php
@@ -44,6 +44,9 @@ class LionMenu {
 	}
 
 	function deactivate() {
+        // TEMP - delete tables on deactivate (for debugging)
+        
+
         // Print message stating that data will not be deleted from database
         // but that there might be issues on your website.
         
