32b4401b7121cc80f2dcfa822570ee2a860c730a
admin <alex.lord9@hotmail.co.uk>
2019-01-27 13:24:26 +0000

    [VP] Deactivated plugin 'Lion Menu'
    VP-Action: plugin/deactivate/lion-menu/lion-menu.php
    VP-Plugin-Name: Lion Menu
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

--- wp-content/plugins/lion-menu/assets/sql/create_menu_table.sql
+++ wp-content/plugins/lion-menu/assets/sql/create_menu_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tableplaceholder (
     id mediumint(9) NOT NULL AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
-    order int(2) NOT NULL,
+    order int(3) NOT NULL,
     date_created datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
     date_updated datetime, 
     author int(3) NOT NULL,
