b39b368bb89eb7e8c5b4d247b796938cb1e38022
admin <alex.lord9@hotmail.co.uk>
2019-01-27 19:44:39 +0000

    Moved PHP POST handle above HTML so it doesn't render HTML if POSTING

--- wp-content/plugins/lion-menu/templates/admin/lm-header.php
+++ wp-content/plugins/lion-menu/templates/admin/lm-header.php
@@ -1,3 +1,29 @@
+<?php
+
+/**
+ * When 'Add' is Clicked in Modal
+ * Insert a New Menu into Database
+ */
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    require_once( WP_PLUGIN_DIR . '/lion-menu/includes/lm-sql-manager.class.php' );
+
+    if(!isset($_POST["menu-name"])) {
+        return;
+    }
+
+    $params = array(
+        'name' => $_POST["menu-name"], 
+        'date_created' => current_time( 'mysql' ), 
+        'author' => get_current_user_id()
+    );
+
+    $db = new SQLManager();
+    $db->insert("menu", $params);
+}
+
+?>
+
 <h1>Menu</h1>
 <p>Create and manage menu's from this page. Click 'Add Menu' below to create a new menu. Select a menu from the list below to edit a menu.</p> 
 
@@ -19,25 +45,3 @@
 </button>
 
 <br />
-
-<?php
-
-/**
- * When 'Add' is Clicked in Modal
- * Insert a New Menu into Database
- */
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    require_once( WP_PLUGIN_DIR . '/lion-menu/includes/lm-sql-manager.class.php' );
-
-    $params = array(
-        'name' => $_POST["menu-name"], 
-        'date_created' => current_time( 'mysql' ), 
-        'author' => get_current_user_id()
-    );
-
-    $db = new SQLManager();
-    $db->insert("menu", $params);
-}
-
-?>
