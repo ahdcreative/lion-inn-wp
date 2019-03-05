805b1cca5f13b4d3ef8fb6e4ebdae320d934dbc2
admin <alex.lord9@hotmail.co.uk>
2019-03-05 20:02:02 +0000

    [VP] Deactivated plugin 'Lion Menu'
    VP-Action: plugin/deactivate/lion-menu/lion-menu.php
    VP-Plugin-Name: Lion Menu
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

--- wp-content/plugins/lion-menu/assets/sql/insert_test_data.sql
+++ wp-content/plugins/lion-menu/assets/sql/insert_test_data.sql
@@ -14,23 +14,24 @@ INSERT INTO `hjb01_lm_section` (`id`, `name`, `rank`, `date_created`, `date_upda
 (9, 'Desserts', 3, '2019-02-19 13:35:46', NULL, 1, NULL, 0, 14),
 (11, 'Coffee', 0, '2019-02-24 19:18:09', NULL, 1, NULL, 1, 14);
 
-INSERT INTO `hjb01_lm_item` (`id`, `name`, `rank`, `date_created`, `date_updated`, `author`, `editor`, `price`, `description`, `isVegetarian`, `isGlutenFree`, `isSubsectionTitle`, `parent_section`) VALUES
-(2, 'Soup of the Day', 2, '2019-01-30 22:19:32', NULL, 1, NULL, '5.00', 'Homemade soup of the day served with hot crusty bread.', 1, 1, 0, 1),
-(3, 'Ham, Egg & Chips', 3, '2019-01-30 22:20:14', '2019-02-12 19:42:53', 1, 1, '0.00', 'Gammon ham served with egg and chips.', 0, 1, 0, 2),
-(4, 'Pie', 2, '2019-01-30 22:20:14', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 2),
-(5, 'Chocolate', 1, '2019-01-30 22:21:02', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 4),
-(6, 'Vanilla', 2, '2019-01-30 22:21:02', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 4),
-(7, 'Filter Coffee', 1, '2019-01-30 22:21:38', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 5),
-(8, 'Tea', 2, '2019-01-30 22:21:38', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 5),
-(9, 'Margharita', 1, '2019-01-30 22:22:31', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 6),
-(10, 'Hawaiin', 2, '2019-01-30 22:22:31', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 6),
-(11, 'Mushroom', 1, '2019-01-30 22:23:03', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 7),
-(12, 'Onion', 2, '2019-01-30 22:23:03', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 7),
-(13, 'Chips', 1, '2019-01-30 22:23:34', '2019-02-19 00:14:21', 1, 1, '3.50', '', 0, 0, 0, 3),
-(14, 'Salad', 2, '2019-01-30 22:23:34', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 3),
-(17, 'Pork Crackling', 1, '2019-02-10 20:52:52', '2019-02-12 20:51:24', 1, 1, '0.00', 'Served with apple sauce.', 1, 1, 0, 1),
-(18, 'Pasta', 1, '2019-02-12 20:01:14', NULL, 1, NULL, '0.00', '', 0, 0, 1, 2),
-(20, 'Cheese & Biscuits', 1, '2019-02-24 18:12:49', NULL, 1, NULL, '4.00', '', 1, 0, 0, 9);
+INSERT INTO `hjb01_lm_item` (`id`, `name`, `rank`, `date_created`, `date_updated`, `author`, `editor`, `price`, `type`, `description`, `isVegetarian`, `isGlutenFree`, `parent_section`) VALUES
+(2, 'Soup of the Day', 2, '2019-01-30 22:19:32', NULL, 1, NULL, '5.00', 'item', 'Homemade soup of the day served with hot crusty bread.', 1, 1, 1),
+(3, 'Ham, Egg & Chips', 3, '2019-01-30 22:20:14', '2019-02-12 19:42:53', 1, 1, '0.00', 'item', 'Gammon ham served with egg and chips.', 0, 1, 2),
+(4, 'Pie', 2, '2019-01-30 22:20:14', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 2),
+(5, 'Chocolate', 1, '2019-01-30 22:21:02', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 4),
+(6, 'Vanilla', 2, '2019-01-30 22:21:02', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 4),
+(7, 'Filter Coffee', 1, '2019-01-30 22:21:38', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 5),
+(8, 'Tea', 2, '2019-01-30 22:21:38', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 5),
+(9, 'Margharita', 1, '2019-01-30 22:22:31', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 6),
+(10, 'Hawaiin', 2, '2019-01-30 22:22:31', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 6),
+(11, 'Mushroom', 1, '2019-01-30 22:23:03', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 7),
+(12, 'Onion', 2, '2019-01-30 22:23:03', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 7),
+(13, 'Chips', 1, '2019-01-30 22:23:34', '2019-02-19 00:14:21', 1, 1, '3.50', 'item', '', 0, 0, 3),
+(14, 'Salad', 2, '2019-01-30 22:23:34', NULL, 1, NULL, NULL, 'item', '', NULL, NULL, 3),
+(17, 'Pork Crackling', 1, '2019-02-10 20:52:52', '2019-02-12 20:51:24', 1, 1, '0.00', 'item', 'Served with apple sauce.', 1, 1, 1),
+(18, 'Pasta', 1, '2019-02-12 20:01:14', NULL, 1, NULL, '0.00', 'subtitle', '', 0, 0, 2),
+(18, 'Note', 5, '2019-02-12 20:01:14', NULL, 1, NULL, '0.00', 'note', '', 0, 0, 2),
+(20, 'Cheese & Biscuits', 1, '2019-02-24 18:12:49', NULL, 1, NULL, '4.00', 'item', '', 1, 0, 9);
 
 
 INSERT INTO `hjb01_lm_subitem` (`id`, `name`, `rank`, `date_created`, `date_updated`, `author`, `editor`, `price`, `parent_item`) VALUES
