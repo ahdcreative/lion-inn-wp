fd53176173f3410acfea1efdb807df3422e24883
admin <alex.lord9@hotmail.co.uk>
2019-03-11 22:07:20 +0000

    Menu formatted correctly in JSON

--- misc/support/menu-scraper/menu-scraper.py
+++ misc/support/menu-scraper/menu-scraper.py
@@ -2,6 +2,7 @@ from lxml import html, etree
 import requests
 from collections import defaultdict
 import json
+import io
 
 # Get HTML source from menu page
 page = requests.get('http://www.lioninn.co.uk/menu.html')
@@ -14,69 +15,70 @@ menu_arias = tree.xpath('//a[contains(@class,"nav-link")]/@aria-controls')
 menu_names = dict(zip(menu_arias, menu_values))
 
 # Entire Menu
-menu = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+# menu = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 
-# Create structure of menu --> sections --> items (item, subtitle, note) --> subitems
+# Create structure of menu --> sections --> items (item, subtitle, note)
+menus = []
+menu = {}
 for aria, menu_name in menu_names.items():
     aria = str(aria)
-    sections = tree.xpath('//div[@id="%s"]//h1/text()' % aria)
-    # Create dict of menu : sections
-    # menu[aria] = sections
+    menu['name'] = aria
+    section_names = tree.xpath('//div[@id="%s"]//h1/text()' % aria)
 
     # Loop through sections
-    for sec in sections:
+    sections = []
+    section = {}
+    for sec in section_names:
         sec = str(sec)
         sec = sec.lower()
         sec = sec.replace(' ', '-')
+        section['name'] = sec
         # Get items
-        item_names = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]//h3/text()' % (aria, sec))
-        item_descs = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]//p[not(@class)]/text()' % (aria, sec))
-        item_prices = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]//p[contains(@class, "price")]/text()' % (aria, sec))
+        if(sec == 'sides'):
+            item_names = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]//h3/text()' % (aria, sec))
+            item_descs = [' '] * 20
+            item_prices = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]//div[not(contains(@class, "text-md-right"))]/p[contains(@class, "price")]/text()' % (aria, sec))
+        else:
+            item_names = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]//h3/text()' % (aria, sec))
+            item_descs = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]//p[not(@class)]/text()' % (aria, sec))
+            item_prices = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]//p[contains(@class, "price")]/text()' % (aria, sec))
 
-        print(len(item_names))
-        print(len(item_descs))
-        print(len(item_prices))
-
+        # Create list of items in given section
         items = []
         item = {}
-        for i in range(max(len(item_names), len(item_descs), len(item_prices))):
+        for i in range(min(len(item_names), len(item_descs), len(item_prices))):
             if i < len(item_names):
                 item['name'] = item_names[i]
             if i < len(item_descs):
                 item['desc'] = item_descs[i]
             if i < len(item_prices):
-                item['price'] = item_prices[i]
+                item['price'] = item_prices[i].strip('\u00a3')
 
-            print(item)
             items.append(item)
-            # item.clear()
+            item = {}
 
-        # print(items)
+        # Append items to section
+        section['items'] = items
+        sections.append(section)
+        section = {}
+    
+    # Append sections to menu
+    menu['sections'] = sections
+    menus.append(menu)
+    menu = {}
 
-        # Create dict within above dict of menu : sections : items
-        # menu[sec_class] = item_names
+# Print to console
+# print(json.dumps(menus, indent=4))
 
-        # Loop through items
-        for item in item_names:
-            item = str(item)
-            # Get subitems
-            subitem_names = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]/div[contains(@class, "sub-item")]//p[not(@class)]/text()' % (aria, sec))      
-            subitem_prices = tree.xpath('//div[@id="%s"]//div[contains(@class, "%s")]/div[contains(@class, "sub-item")]//p[contains(@class, "sub-price")]/text()' % (aria, sec))
-            
-            # print(len(subitem_names))
-            # print(len(subitem_prices))
+# Export to JSON file
+# with open('menu_json.json', 'w') as of:
+#     json.dump(menus, of)
 
-            subitems = []
-            subitem = {}
-            for j in range(max(len(subitem_names), len(subitem_prices))):
-                if j < len(subitem_names):
-                    subitem['name'] = subitem_names[j]
-                if j < len(subitem_prices):
-                    subitem['price'] = subitem_prices[j]
+of = open('menu.txt', 'w')
+of.write("test")
+of.close()
 
-                print(subitem)
-                subitems.append(subitem)
-
-            # Create dict within above dict of menu : sections : items : subitems
-
-# print(json.dumps(menu, indent=4))
+# {
+#     "name": "Bacon",
+#     "price": "+60p"
+# }
