ff71d4e002793806e61b1b1716b516dc33490f17
admin <alex.lord9@hotmail.co.uk>
2019-04-01 22:54:31 +0100

    Refactor events table structure

--- wp-content/plugins/lion-events/assets/sql/create_event_table.sql
+++ wp-content/plugins/lion-events/assets/sql/create_event_table.sql
@@ -2,11 +2,13 @@ CREATE TABLE tableplaceholder (
     id mediumint(9) NOT NULL AUTO_INCREMENT,
     name VARCHAR(40),
     rank int(3) NOT NULL,
+    event_date datetime NOT NULL,
     date_created datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
     date_updated datetime, 
     author int(3) NOT NULL,
     editor int(3),
-    description VARCHAR(400),
+    description_sml VARCHAR(300),
+    description_lrg VARCHAR(800),
     toPublish boolean NOT NULL DEFAULT 1,  
     PRIMARY KEY  (id)
 ) charsetplaceholder;
