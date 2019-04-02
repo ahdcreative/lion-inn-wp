8aef5f3d0f54692f3b2615c66f6c6081f1f74139
admin <alex.lord9@hotmail.co.uk>
2019-04-02 21:12:56 +0100

    Remove separate delete event modal file

--- wp-content/plugins/lion-events/templates/admin/le-delete-modal.php
+++
@@ -1,9 +0,0 @@
-<div id="<?php echo $id; ?>" style="display:none;">
-
-    <form action="#" method="post" class="row d-flex p-3">
-        <h3 class="mb-4">Are you sure you want to delete this?</h3>
-        <input type="hidden" name="<?php echo $name; ?>" /> <br/>
-        <input type="submit" value="Delete" class="btn btn-danger ml-auto" />
-    </form>
-
-</div>
--- wp-content/plugins/lion-events/templates/admin/le-modals.php
+++ wp-content/plugins/lion-events/templates/admin/le-modals.php
@@ -22,10 +22,23 @@
     <form action="#" method="post">
         <h3 class="mb-4">Edit Event</h3>
         <input type="hidden" name="edit-event" />
-
+        <?php echo $tpl->render( 'le-text-input', array( "id" => "event-name-input", "name" => "event-name", "label" => "Event Name", "placeholder" => "Enter Name" )); ?>
+        <?php echo $tpl->render( 'le-text-input', array( "id" => "event-date-input", "name" => "event-date", "label" => "Event Date", "placeholder" => "Enter Date" )); ?>
+        <?php echo $tpl->render( 'le-checkbox-input', array( "id" => "publish-event-check", "name" => "publish-event", "label" => "Publish", "optClasses" => "mb-3" )); ?>
+        <?php echo $tpl->render( 'le-textarea-input', array( "id" => "desc-input-sml", "name" => "event-desc-sml", "label" => "Small Description", "optClasses" => "hideIfSubsec" )); ?>
+        <?php echo $tpl->render( 'le-textarea-input', array( "id" => "desc-input-lrg", "name" => "event-desc-lrg", "label" => "Large Description", "optClasses" => "hideIfSubsec" )); ?>
+        <?php echo $tpl->render( 'le-form-buttons', array( "value" => "Edit" )); ?>
     </form>
 
 </div>
 
 <!-- Delete Menu Modal -->
-<?php echo $tpl->render( 'le-delete-modal', array( "id" => "delete-event-modal", "name" => "delete-event" )); ?>
+<div id="delete-event-modal" style="display:none;">
+
+    <form action="#" method="post" class="row d-flex p-3">
+        <h3 class="mb-4">Are you sure you want to delete this?</h3>
+        <input type="hidden" name="delete-event" /> <br/>
+        <input type="submit" value="Delete" class="btn btn-danger ml-auto" />
+    </form>
+
+</div>
