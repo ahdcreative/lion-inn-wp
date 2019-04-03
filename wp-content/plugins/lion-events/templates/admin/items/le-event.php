e23e8e3e7c8b1d382f68bbbb4b92794d92adfe56
admin <alex.lord9@hotmail.co.uk>
2019-04-03 22:04:37 +0100

    Minor template changes

--- wp-content/plugins/lion-events/templates/admin/items/le-event.php
+++ wp-content/plugins/lion-events/templates/admin/items/le-event.php
@@ -9,9 +9,6 @@
     <!-- Event Name -->
     <span class="ml-3 event-name"><?php echo $name; ?></span>
 
-    <!-- Event Date -->
-    <span class="ml-3 event-date"><?php echo $event_date; ?></span>
-
     <!-- Publish / Not Published Icon -->
     <?php
     if($toPublish) {
@@ -26,6 +23,9 @@
         <?php echo $tpl->render( 'le-icon-link', array( "aClasses" => "edit-event", "modal" => "edit-event-modal", "tooltip" => "Edit", "iClasses" => "fa-edit mr-3", "w" => "600", "h" => "550" )); ?>
         <?php echo $tpl->render( 'le-icon-link', array( "aClasses" => "delete-event", "modal" => "delete-event-modal", "tooltip" => "Delete", "iClasses" => "fa-trash-alt", "w" => "275", "h" => "215" )); ?>
     </div>
+    
+    <!-- Event Date -->
+    <p class="ml-3 event-date"><?php echo $event_date; ?></p>
     
     <!-- Small Description -->
     <p class="desc-sml"><?php echo $description_sml; ?></p>
