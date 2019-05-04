74da17f39d0acd63f07d2bc6d5e2ede6f3de8585
admin <alex.lord9@hotmail.co.uk>
2019-05-04 11:53:17 +0100

    [VP] Rollback to the same state as of 28-Apr-19 14:36:21
    VP-Action: versionpress/rollback/bf60515f15e24d27a61aac07a9bfb6fb32729f38
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

--- wp-content/plugins/lion-events/assets/sql/select_where_event.sql
+++ wp-content/plugins/lion-events/assets/sql/select_where_event.sql
@@ -1,3 +1,3 @@
-SELECT id, name, event_start_date, event_end_date, isSingleDayEvent, description_sml, description_lrg, toPublish 
+SELECT id, name, event_start_date, event_end_date, image_url, isSingleDayEvent, description_sml, description_lrg, toPublish 
 FROM prefixplaceholder_event
 WHERE where_placeholder;
