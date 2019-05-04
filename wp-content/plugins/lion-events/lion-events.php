02754072e87c9cb997a672768986f8c557454079
admin <alex.lord9@hotmail.co.uk>
2019-05-04 11:52:46 +0100

    [VP] Activated plugin 'Lion Events'
    VP-Action: plugin/activate/lion-events/lion-events.php
    VP-Plugin-Name: Lion Events
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

--- wp-content/plugins/lion-events/lion-events.php
+++ wp-content/plugins/lion-events/lion-events.php
@@ -80,9 +80,9 @@ class LionEvents {
         // Font Awesome
         wp_enqueue_style('fa-icons', 'https://use.fontawesome.com/releases/v5.6.3/css/all.css');
 
-        // // Enqueue WordPress media scripts
-        // wp_enqueue_media();
-        // // Enqueue custom script that will interact with wp.media
+        // Enqueue WordPress media scripts
+        wp_enqueue_media();
+        // Enqueue custom script that will interact with wp.media
         wp_enqueue_script( 'le-media-manager', plugins_url( '/js/media-manager.js' , __FILE__ ), array('jquery'), '0.1' );
     }
     
