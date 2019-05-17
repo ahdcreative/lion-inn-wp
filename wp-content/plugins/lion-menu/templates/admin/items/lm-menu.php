c57426daca7aad5261602c8cf457b752d916a943
Alex Lord <Alexander2.Lord@live.uwe.ac.uk>
2019-05-17 19:13:14 +0100

    published icon next to other icons

--- wp-content/plugins/lion-menu/templates/admin/items/lm-menu.php
+++ wp-content/plugins/lion-menu/templates/admin/items/lm-menu.php
@@ -11,17 +11,16 @@
     <!-- Menu Name -->
     <a href="admin.php?page=lm-menu-edit-subpage&menu_id=<?php echo $id ?>" class="ml-3 menu-name"><?php echo $name; ?></a>
 
-    <!-- Publish / Not Published Icon -->
-    <?php
-    if($toPublish) {
-        echo $tpl->render( 'lm-icon', array( "classes" => "fas fa-check-circle toPublish ml-2", "tooltip" => "Published"));
-    } else {
-        echo $tpl->render( 'lm-icon', array( "classes" => "fas fa-times-circle ml-2", "tooltip" => "Not Published"));
-    }
-    ?>
-
     <!-- Icons -->
     <div class='float-right'>
+        <!-- Publish / Not Published Icon -->
+        <?php
+        if($toPublish) {
+            echo $tpl->render( 'lm-icon', array( "classes" => "fas fa-check-circle toPublish mr-3", "tooltip" => "Published"));
+        } else {
+            echo $tpl->render( 'lm-icon', array( "classes" => "fas fa-times-circle mr-3", "tooltip" => "Not Published"));
+        }
+        ?>
         <?php echo $tpl->render( 'lm-icon-link', array( "aClasses" => "edit-menu", "modal" => "edit-menu-modal", "tooltip" => "Edit", "iClasses" => "fa-edit mr-3", "w" => "400", "h" => "250" )); ?>
         <?php echo $tpl->render( 'lm-icon-link', array( "aClasses" => "delete-menu", "modal" => "delete-menu-modal", "tooltip" => "Delete", "iClasses" => "fa-trash-alt", "w" => "275", "h" => "215" )); ?>
     </div>
