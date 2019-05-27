cdd2bd18fcb7b2dd49fedee338b37727f0030f8e
Alex Lord <Alexander2.Lord@live.uwe.ac.uk>
2019-05-27 15:53:43 +0100

    imported docs as PDFs

--- wp-content/plugins/lion-docs/templates/ld-docs.php
+++ wp-content/plugins/lion-docs/templates/ld-docs.php
@@ -2,24 +2,18 @@
 $docs = new LDTemplate( __DIR__ );
 $nav = new LDTemplate( __DIR__ . '/nav' );
 
-$pdf = plugins_url() . '/lion-docs/docs/test.pdf';
-$pdf2 = plugins_url() . '/lion-docs/docs/test2.pdf';
-
-// To go down below 
+$default = plugins_url() . '/lion-docs/docs/pdf/general/accessing-the-admin-panel.pdf';
 ?>
 
 <div class='container-fluid'>
     <div class='row py-3'>
         <div class='col-2'>
             <h3>Side Navigation</h3>
-
-            <?php echo $nav->render('ld-nav-link', array('filename' => $pdf, 'title' => 'Test 1')); ?>
-            <?php echo $nav->render('ld-nav-link', array('filename' => $pdf2, 'title' => 'Test 2')); ?>
             
             <?php echo $nav->render('ld-nav'); ?>
         </div>
         <div class='col' id='main'>
-            <?php echo $docs->render('ld-iframe', array('pdf' => $pdf)); ?>
+            <?php echo $docs->render('ld-iframe', array('pdf' => $default)); ?>
         </div>
     </div>
 </div>
--- wp-content/plugins/lion-docs/templates/nav/ld-nav.php
+++ wp-content/plugins/lion-docs/templates/nav/ld-nav.php
@@ -1,15 +1,4 @@
-<?php
-
-// equivalent of list template file
-// render all doc names with links to their files
-
-// i.e.
-// $pdf2 = plugins_url() . '/lion-docs/docs/test2.pdf';
-// foreach() {
-//      echo $nav->render('ld-nav-link', array('filename' => $pdf2, 'title' => 'Test 2'));
-// }
-
-?>
+<?php $nav = new LDTemplate( __DIR__ ); ?>
 
 <div id="accordion">
     <div id="general-docs">
@@ -20,7 +9,26 @@
         </div>
 
         <div id="collapse-general" class="collapse show ml-4" aria-labelledby="general-heading" data-parent="#accordion">
-            Doc
+            <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/accessing-the-admin-panel.pdf', 'title' => 'Accessing the Admin Panel')); ?>
+        
+            Admin Panel Overview
+            <div class="ml-4">
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/admin-panel-overview-intro.pdf', 'title' => 'Introduction')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/admin-panel-overview-going-to-different-pages.pdf', 'title' => 'Going to Different Pages')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/admin-panel-overview-view-actual-website.pdf', 'title' => 'View the Actual Website')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/admin-panel-overview-logout.pdf', 'title' => 'Logout')); ?>
+            </div>
+
+            Managing Users
+            <div class="ml-4">
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/managing-users-intro.pdf', 'title' => 'Introduction')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/managing-users-add-user.pdf', 'title' => 'Add New User')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/managing-users-delete-user.pdf', 'title' => 'Delete User')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/managing-users-user-roles.pdf', 'title' => "User 'Roles'" )); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/managing-users-editing-roles.pdf', 'title' => 'Editing Roles')); ?>
+            </div>
+            
+            <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/the-text-editor.pdf', 'title' => 'The Text Editor')); ?>
         </div>
     </div>
 
@@ -31,8 +39,59 @@
             </a>
         </div>
 
-        <div id="collapse-menu" class="collapse show ml-4" aria-labelledby="menu-heading" data-parent="#accordion">
-            Doc
+        <div id="collapse-menu" class="collapse hide ml-4" aria-labelledby="menu-heading" data-parent="#accordion">
+            Overview
+            <div class="ml-4">
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/overview-access-menus.pdf', 'title' => 'Access the Menu\'s')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/overview-menu-section-item-subitem.pdf', 'title' => 'Menu, Section, Item, Subitem')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/overview-published-not-published.pdf', 'title' => 'Published & Not Published')); ?>
+            </div>
+
+            Adding
+            <div class="ml-4">
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/add-menu.pdf', 'title' => 'Add Menu')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/add-section.pdf', 'title' => 'Add Section')); ?>
+                <div class="ml-4">
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/add-section-to-the-right.pdf', 'title' => 'Add to the Right' )); ?>
+                </div>
+                Add Item
+                <div class="ml-4">
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/add-item-food.pdf', 'title' => 'Food Item' )); ?>
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/add-item-subtitle.pdf', 'title' => 'Subtitle Item' )); ?>
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/add-item-note.pdf', 'title' => 'Note Item' )); ?>
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/add-item-how-they-look.pdf', 'title' => 'How These Look on the Website' )); ?>
+                </div>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/add-subitem.pdf', 'title' => 'Add Subitem')); ?>
+            </div>
+
+            Editing
+            <div class="ml-4">
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/edit-menu.pdf', 'title' => 'Edit Menu')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/edit-section.pdf', 'title' => 'Edit Section')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/edit-item.pdf', 'title' => 'Edit Item')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/edit-subitem.pdf', 'title' => 'Edit Subitem')); ?>
+            </div>
+
+            Deleting
+            <div class="ml-4">
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/delete-menu.pdf', 'title' => 'Delete Menu')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/delete-section.pdf', 'title' => 'Delete Section')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/delete-item.pdf', 'title' => 'Delete Item')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/delete-subitem.pdf', 'title' => 'Delete Subitem')); ?>
+            </div>
+
+            Reordering
+            <div class="ml-4">
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/reorder-menus.pdf', 'title' => 'Reorder Menu\'s')); ?>
+                Reordering Menu Content (Sections, Items & Subitems)
+                <div class="ml-4">
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/reorder-content-basic-usage.pdf', 'title' => 'Basic Usage' )); ?>
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/reorder-content-up-and-down.pdf', 'title' => 'Moving Up & Down' )); ?>
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/reorder-content-sections.pdf', 'title' => 'Moving Sections' )); ?>
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/reorder-content-items.pdf', 'title' => 'Moving Items' )); ?>
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/menu/reorder-content-moving-sides.pdf', 'title' => 'Moving Sides' )); ?>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -43,8 +102,28 @@
             </a>
         </div>
 
-        <div id="collapse-event" class="collapse show ml-4" aria-labelledby="event-heading" data-parent="#accordion">
-            Doc
+        <div id="collapse-event" class="collapse hide ml-4" aria-labelledby="event-heading" data-parent="#accordion">
+            Regular Events
+            <div class="ml-4">
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/regular-events-overview.pdf', 'title' => 'Overview')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/regular-events-view-event-info.pdf', 'title' => 'View Event Information')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/regular-events-edit-event-info.pdf', 'title' => 'Edit Event Information')); ?>
+                <div class="ml-4">
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/regular-events-change-icon.pdf', 'title' => 'Changing the Icon')); ?>
+                </div>
+            </div>
+
+            Upcoming Events
+            <div class="ml-4">
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/upcoming-events-overview.pdf', 'title' => 'Introduction')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/upcoming-events-add-event.pdf', 'title' => 'Add New Event')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/upcoming-events-edit-event.pdf', 'title' => 'Edit Event')); ?>
+                <div class="ml-4">
+                    <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/upcoming-events-change-image.pdf', 'title' => 'Change the Image' )); ?>
+                </div>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/upcoming-events-delete-event.pdf', 'title' => 'Delete Event')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/events/upcoming-events-published-not-published.pdf', 'title' => 'Published & Not Published')); ?>
+            </div>
         </div>
     </div>
 </div>
