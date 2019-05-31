2578232560d40a3baa05038afa65eb5633e8662f
Alex Lord <Alexander2.Lord@live.uwe.ac.uk>
2019-05-31 16:35:21 +0100

    make sure u-events are organized by newest date

--- wp-content/plugins/lion-events/assets/sql/select_where_u_event.sql
+++ wp-content/plugins/lion-events/assets/sql/select_where_u_event.sql
@@ -1,3 +1,4 @@
 SELECT id, name, event_start_date, event_end_date, image_url, image_height, image_width, isSingleDayEvent, description, toPublish 
 FROM prefixplaceholder_u_event
-WHERE where_placeholder;
+WHERE where_placeholder
+ORDER BY event_start_date ASC;
